332b85703087ce7fd0e797cf69a4de5d2f55481b
Allan <allanpaiva17@gmail.com>
2023-02-15 18:17:39 -0300

    feat: mux package added

--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/lanpaiva/person-api-rest/controller"
 )
 
 func HandleResponse() {
-	http.HandleFunc("/", controller.HandlerRequest)
-	http.HandleFunc("/persons", controller.AllPersons)
-	defer log.Fatal(http.ListenAndServe(":8080", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controller.HandlerRequest)
+	r.HandleFunc("/persons", controller.AllPersons)
+	defer log.Fatal(http.ListenAndServe(":8080", r))
 }
